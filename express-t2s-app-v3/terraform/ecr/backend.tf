96b4ecc6ce26be94eb04186aad109ca1156f9b67
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-05 14:23:45 -0500

    Update backend.tf

--- express-t2s-app-v3/terraform/ecr/backend.tf
+++ express-t2s-app-v3/terraform/ecr/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "emmanuel-tf-state"      # Your S3 bucket name
-    key            = "ecs/terraform.tfstate"  # For ECS state file (change to eks/... for EKS)
+    key            = "ecr/terraform.tfstate"  # For ECS state file (change to eks/... for EKS)
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"        # Your DynamoDB lock table
     encrypt        = true
