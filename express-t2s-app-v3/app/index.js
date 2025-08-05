160faf166cf2e52e888690b1b36908d9f2c9664a
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-05 15:46:18 -0500

    Update index.js

--- express-t2s-app-v3/app/index.js
+++ express-t2s-app-v3/app/index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello from Express App on ECS!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`App is running on port ${PORT}`);
 });
