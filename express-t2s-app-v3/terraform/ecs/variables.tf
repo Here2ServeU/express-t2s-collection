daf57794c9ddf6b3d64567b62c453a9de5f0d06b
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-06 15:01:56 -0500

    Update variables.tf

--- express-t2s-app-v3/terraform/ecs/variables.tf
+++ express-t2s-app-v3/terraform/ecs/variables.tf
@@ -1,7 +1,18 @@
-variable "region" {
-  description = "AWS region"
-}
 
-variable "cluster_name" {
-  description = "ECS Cluster Name"
+variable "region" {}
+variable "cluster_name" {}
+variable "task_execution_role_name" {}
+variable "task_execution_policy_arn" {}
+variable "sg_name" {}
+variable "sg_description" {}
+variable "vpc_id" {}
+variable "image_url" {}
+variable "task_family" {}
+variable "task_cpu" {}
+variable "task_memory" {}
+variable "container_name" {}
+variable "service_name" {}
+variable "desired_count" {}
+variable "subnet_ids" {
+  type = list(string)
 }
