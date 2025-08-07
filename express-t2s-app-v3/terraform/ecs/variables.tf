c5596b2ff4b7363b01987c1e2d0d2e4461540563
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-07 13:50:46 -0500

    Update variables.tf

--- express-t2s-app-v3/terraform/ecs/variables.tf
+++ express-t2s-app-v3/terraform/ecs/variables.tf
@@ -1,18 +1,19 @@
-
 variable "region" {}
+variable "repo_name" {}
 variable "cluster_name" {}
 variable "task_execution_role_name" {}
-variable "task_execution_policy_arn" {}
+variable "task_execution_policy_arn" {
+  default = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
 variable "sg_name" {}
 variable "sg_description" {}
 variable "vpc_id" {}
-variable "image_url" {}
+variable "subnet_ids" {
+  type = list(string)
+}
 variable "task_family" {}
 variable "task_cpu" {}
 variable "task_memory" {}
 variable "container_name" {}
 variable "service_name" {}
 variable "desired_count" {}
-variable "subnet_ids" {
-  type = list(string)
-}
