15f607e10bd05fc631789055d042855eec3301e5
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-28 18:48:01 -0500

    Create variables.tf

---
+++ express-t2s-app-v3/terraform/eks/modules/vpc/variables.tf
@@ -0,0 +1,5 @@
+variable "cluster_name" { type = string }
+variable "env"         { type = string }
+variable "type"        { type = string }
+variable "region"      { type = string }
+variable "cidr_block"  { type = string }
