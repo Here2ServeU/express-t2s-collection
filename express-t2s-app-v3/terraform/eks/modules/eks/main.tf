714320cb76269353e15e7d9750b6c1e0e3d267e9
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-28 18:42:01 -0500

    Create main.tf

---
+++ express-t2s-app-v3/terraform/eks/modules/eks/main.tf
@@ -0,0 +1,72 @@
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
+
+  version = var.cluster_version
+
+  vpc_config {
+    subnet_ids         = var.subnet_ids_private
+    security_group_ids = [var.cluster_security_group_id]
+    endpoint_public_access = true
+    endpoint_private_access = false
+  }
+
+  tags = {
+    Name = var.cluster_name
+  }
+}
+
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.cluster_name}-default"
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = var.subnet_ids_private
+
+  scaling_config {
+    desired_size = var.desired_size
+    min_size     = var.min_size
+    max_size     = var.max_size
+  }
+
+  instance_types = var.instance_types
+
+  depends_on = [aws_eks_cluster.this]
+}
+
+# Add-ons
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name  = aws_eks_cluster.this.name
+  addon_name    = "vpc-cni"
+  addon_version = var.vpc_cni_version
+  depends_on    = [aws_eks_cluster.this]
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name  = aws_eks_cluster.this.name
+  addon_name    = "kube-proxy"
+  addon_version = var.kube_proxy_version
+  depends_on    = [aws_eks_cluster.this]
+}
+
+# OIDC provider for IRSA
+data "tls_certificate" "oidc" {
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "this" {
+  url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.oidc.certificates[0].sha1_fingerprint]
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.this.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
+
+output "oidc_provider_arn" {
+  value = aws_iam_openid_connect_provider.this.arn
+}
