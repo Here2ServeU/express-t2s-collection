5620f0aa363a004b656b28ecea2a583ba7658a35
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-28 18:49:18 -0500

    Update provider.tf

--- express-t2s-app-v3/terraform/eks/provider.tf
+++ express-t2s-app-v3/terraform/eks/provider.tf
@@ -1,14 +1,6 @@
-# configure aws provider
 provider "aws" {
   region = var.region
 }
 
-# configure backend
-terraform {
-  backend "s3" {
-    bucket         = "t2s-s3-08252025"
-    key            = "aws-eks.terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "my-lock-table"
-  }
-}
+# After the EKS cluster is created, these providers will be configured
+# using the cluster endpoint & auth (see data sources below).
