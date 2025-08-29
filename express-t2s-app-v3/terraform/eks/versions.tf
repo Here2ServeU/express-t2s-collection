e30467ddc750d0ccb458c112ceb5992bc20fdefd
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-29 13:55:40 -0500

    Update versions.tf

--- express-t2s-app-v3/terraform/eks/versions.tf
+++ express-t2s-app-v3/terraform/eks/versions.tf
@@ -1,18 +1,20 @@
 terraform {
   required_version = ">= 1.6.0"
-
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.48"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.29"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.13"
-    }
+    aws = { source = "hashicorp/aws", version = "~> 5.50" }
+    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.29" }
+    helm = { source = "hashicorp/helm", version = "~> 2.13" }
   }
+
+  backend "s3" {
+    bucket         = "YOUR_S3_BUCKET"
+    key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "YOUR_DDB_TABLE"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.region
 }
