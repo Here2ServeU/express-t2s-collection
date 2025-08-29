34a3fb59d6f36ae2028454dc35af0220b7228f55
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-29 13:57:22 -0500

    Update main.tf

--- express-t2s-app-v3/terraform/eks/main.tf
+++ express-t2s-app-v3/terraform/eks/main.tf
@@ -1,5 +1,5 @@
 ##########################
-# Root Modules Wiring
+# Modules
 ##########################
 
 module "vpc" {
@@ -20,9 +20,9 @@ module "security_groups" {
 }
 
 module "iam" {
-  source = "./modules/iam"
-
-  cluster_name = var.cluster_name
+  source        = "./modules/iam"
+  cluster_name  = var.cluster_name
+  oidc_provider_arn = module.eks.oidc_provider_arn  # wired after eks created via depends_on
 }
 
 module "eks" {
@@ -30,52 +30,67 @@ module "eks" {
 
   cluster_name               = var.cluster_name
   cluster_version            = var.cluster_version
+
   subnet_ids_private         = module.vpc.private_subnet_ids
   cluster_security_group_id  = module.security_groups.cluster_sg_id
+
   cluster_role_arn           = module.iam.cluster_role_arn
   node_role_arn              = module.iam.node_role_arn
+
   desired_size               = var.node_desired_size
   min_size                   = var.node_min_size
   max_size                   = var.node_max_size
   instance_types             = var.node_instance_types
+
   vpc_cni_version            = var.vpc_cni_version
   kube_proxy_version         = var.kube_proxy_version
+
+  depends_on = [module.vpc, module.security_groups] # ensure VPC/SGs first
 }
 
-# EKS cluster connection data (used to configure helm/k8s providers)
+# Read AFTER cluster is created
 data "aws_eks_cluster" "this" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
+# Aliased providers pointed at the live cluster (no kubeconfig needed)
 provider "kubernetes" {
+  alias                  = "eks"
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this.token
+  load_config_file       = false
 }
 
 provider "helm" {
+  alias = "eks"
   kubernetes {
     host                   = data.aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.this.token
+    load_config_file       = false
   }
 }
 
+# ALB Controller (after cluster ready)
 module "alb" {
   source = "./modules/alb"
 
-  cluster_name              = module.eks.cluster_name
-  region                    = var.region
-  vpc_id                    = module.vpc.vpc_id
-  oidc_provider_arn         = module.eks.oidc_provider_arn
+  cluster_name      = module.eks.cluster_name
+  region            = var.region
+  vpc_id            = module.vpc.vpc_id
+  oidc_provider_arn = module.eks.oidc_provider_arn
 
-  # Let this module use the helm/kubernetes providers configured above
   providers = {
-    helm       = helm
-    kubernetes = kubernetes
+    helm       = helm.eks
+    kubernetes = kubernetes.eks
   }
+
+  depends_on = [module.eks, module.iam]
 }
