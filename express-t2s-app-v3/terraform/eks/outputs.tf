904c276c7f3819fd94b20fa383aaf81353dd3f58
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-29 13:56:47 -0500

    Update outputs.tf

--- express-t2s-app-v3/terraform/eks/outputs.tf
+++ express-t2s-app-v3/terraform/eks/outputs.tf
@@ -1,19 +1,7 @@
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "private_subnet_ids" {
-  value = module.vpc.private_subnet_ids
-}
-
-output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
-}
+output "cluster_name"           { value = module.eks.cluster_name }
+output "cluster_endpoint"       { value = module.eks.cluster_endpoint }
+output "cluster_oidc_provider"  { value = module.eks.oidc_provider_arn }
+output "vpc_id"                 { value = module.vpc.vpc_id }
+output "public_subnet_ids"      { value = module.vpc.public_subnet_ids }
+output "private_subnet_ids"     { value = module.vpc.private_subnet_ids }
+output "alb_controller_role_arn"{ value = module.iam.alb_controller_role_arn }
