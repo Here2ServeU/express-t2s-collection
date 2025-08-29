ece33c4112f3da87a7aea04508c0b43a64b088a6
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-29 14:30:03 -0500

    Update variables.tf

--- express-t2s-app-v3/terraform/ecr/variables.tf
+++ express-t2s-app-v3/terraform/ecr/variables.tf
@@ -1,7 +1,5 @@
-variable "region" {
-  description = "AWS region"
-}
-
-variable "repo_name" {
-  description = "Repository name"
-}
+variable "region"         { type = string default = "us-east-1" }
+variable "repo_name"      { type = string }
+variable "image_tag"      { type = string default = "latest" }
+variable "scan_on_push"   { type = bool   default = true }
+variable "lifecycle_keep" { type = number default = 10 }
