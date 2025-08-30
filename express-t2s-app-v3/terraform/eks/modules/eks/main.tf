af6caac89a364c9bd298ded99b8763726aa954e3
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-29 19:00:19 -0500

    Update main.tf

--- express-t2s-app-v3/terraform/eks/modules/eks/main.tf
+++ express-t2s-app-v3/terraform/eks/modules/eks/main.tf
@@ -1,1 +1,34 @@
-# main.tf for eks module or chart
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+
+  version = var.cluster_version
+}
+
+resource "aws_eks_node_group" "default" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.cluster_name}-node-group"
+  node_role_arn   = var.node_group_role_arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = var.node_group_desired_size
+    max_size     = var.node_group_max_size
+    min_size     = var.node_group_min_size
+  }
+
+  instance_types = var.node_group_instance_types
+}
+
+output "eks_cluster_name" {
+  value = aws_eks_cluster.this.name
+}
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
+
