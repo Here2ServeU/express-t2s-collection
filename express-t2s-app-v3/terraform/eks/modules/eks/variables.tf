fb6b47542e7a45b9bbff54789eb92381723de096
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-29 19:00:54 -0500

    Update variables.tf

--- express-t2s-app-v3/terraform/eks/modules/eks/variables.tf
+++ express-t2s-app-v3/terraform/eks/modules/eks/variables.tf
@@ -1,1 +1,36 @@
-# variables.tf for eks module or chart
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_version" {
+  type = string
+}
+
+variable "cluster_role_arn" {
+  type = string
+}
+
+variable "node_group_role_arn" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "node_group_instance_types" {
+  type = list(string)
+}
+
+variable "node_group_desired_size" {
+  type = number
+}
+
+variable "node_group_max_size" {
+  type = number
+}
+
+variable "node_group_min_size" {
+  type = number
+}
+
