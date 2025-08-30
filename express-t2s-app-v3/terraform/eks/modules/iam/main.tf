3dd807608b629e24b7640385609305569ca63afd
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-29 19:01:21 -0500

    Update main.tf

--- express-t2s-app-v3/terraform/eks/modules/iam/main.tf
+++ express-t2s-app-v3/terraform/eks/modules/iam/main.tf
@@ -1,1 +1,53 @@
-# main.tf for iam module or chart
+resource "aws_iam_role" "eks_cluster" {
+  name = "t2s-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+resource "aws_iam_role" "eks_node_group" {
+  name = "t2s-eks-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_group.name
+}
+
+output "eks_cluster_role_arn" {
+  value = aws_iam_role.eks_cluster.arn
+}
+
+output "eks_node_group_role_arn" {
+  value = aws_iam_role.eks_node_group.arn
+}
+
