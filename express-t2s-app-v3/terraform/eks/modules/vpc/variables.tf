2fd41ebf31af0f1eef5d586b74c621449c785917
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-08-29 19:03:30 -0500

    Update variables.tf

--- express-t2s-app-v3/terraform/eks/modules/vpc/variables.tf
+++ express-t2s-app-v3/terraform/eks/modules/vpc/variables.tf
@@ -1,1 +1,20 @@
-# variables.tf for vpc module or chart
+variable "vpc_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
