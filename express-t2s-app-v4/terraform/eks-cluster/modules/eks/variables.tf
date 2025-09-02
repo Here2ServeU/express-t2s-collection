652704e453d6de3bd6fce9c44a503adfb7480582
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-09-02 16:44:03 -0500

    Delete express-t2s-app-v4/terraform/eks-cluster/modules directory

--- express-t2s-app-v4/terraform/eks-cluster/modules/eks/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "cluster_name" {}
-variable "region" {}
-variable "vpc_id" {}
-variable "subnet_ids" {}
-variable "control_plane_subnet_ids" {}
-variable "node_group_instance_types" {}
-variable "node_group_capacity_type" {}
-variable "tags" {}
--- express-t2s-app-v4/terraform/eks-cluster/modules/vpc/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "name" {}
-variable "region" {}
-variable "vpc_cidr" {}
-variable "azs" {}
-variable "public_subnets" {}
-variable "private_subnets" {}
-variable "intra_subnets" {}
-variable "tags" {}
