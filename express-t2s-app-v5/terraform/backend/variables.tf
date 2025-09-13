aa7247c125ee7e4773e67354a6f509ca48ae32b4
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-09-13 17:16:15 -0500

    Update variables.tf

--- express-t2s-app-v5/terraform/backend/variables.tf
+++ express-t2s-app-v5/terraform/backend/variables.tf
@@ -1,19 +1,23 @@
 variable "bucket_name" {
   description = "The exact name of the S3 bucket for Terraform backend"
   type        = string
+  default     = "emmanuel-tf-state-09112025"
 }
 
 variable "lock_table" {
   description = "The name of the DynamoDB table for Terraform state locking"
   type        = string
+  default     = "terraform-locks"
 }
 
 variable "aws_region" {
   description = "AWS region"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "environment" {
   description = "Environment tag (e.g., dev, prod)"
   type        = string
+  default     = "dev"
 }
