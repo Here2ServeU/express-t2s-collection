fcabb9555bafabf10694808af1d71f4c23e8c5b5
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-09-13 17:16:53 -0500

    Update variables.tf

--- express-t2s-app-v5/terraform/ecr/variables.tf
+++ express-t2s-app-v5/terraform/ecr/variables.tf
@@ -1,7 +1,11 @@
 variable "region" {
   description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "repo_name" {
   description = "Repository name"
+  type        = string
+  default     = "express-web-app"
 }
