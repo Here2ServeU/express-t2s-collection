dcc04d7a2d1a5f78a307e01d010465d567b36c56
T2S Student <beginner2master2025@gmail.com>
2025-12-06 12:38:54 -0600

    Updated terraform/backend for v3

--- express-t2s-app-v3/terraform/backend/main.tf
+++ express-t2s-app-v3/terraform/backend/main.tf
@@ -1,7 +1,32 @@
-terraform {
-  backend "s3" {
-    bucket = "my-terraform-backend"
-    key    = "ecs/terraform.tfstate"
-    region = "us-east-1"
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "tf_backend" {
+  bucket        = var.bucket_name
+  force_destroy = true
+
+  tags = {
+    Name        = "Terraform Backend Bucket"
+    Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tf_backend_versioning" {
+  bucket = aws_s3_bucket.tf_backend.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = var.lock_table
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
