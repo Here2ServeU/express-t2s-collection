c715f3e59ee4ff1ae70ccb2138dfd9de098f8012
Emmanuel Naweji <revnaweji@gmail.com>
2026-02-13 17:34:51 -0600

    Delete express-t2s-app-v5 directory (#21)

--- express-t2s-app-v5/charts/aiops/templates/_helpers.tpl
+++
@@ -1,7 +0,0 @@
-{{- define "aiops.name" -}}
-aiops-api
-{{- end }}
-
-{{- define "aiops.fullname" -}}
-{{ include "aiops.name" . }}
-{{- end }}
--- express-t2s-app-v5/sre/chaos/pod-kill-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Chaos Test: Kill a random pod
-
-POD=$(kubectl get pods -n default -o name | shuf -n 1)
-
-echo "Killing pod: $POD"
-
-kubectl delete $POD --force --grace-period=0
