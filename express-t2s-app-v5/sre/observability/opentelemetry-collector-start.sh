270f901f0af7d9f604289d926b583523a9d7c576
Emmanuel Naweji <revnaweji@gmail.com>
2026-02-13 17:34:40 -0600

    Delete express-t2s-app-v5 directory

--- express-t2s-app-v5/sre/observability/opentelemetry-collector-start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-kubectl apply -f https://raw.githubusercontent.com/open-telemetry/opentelemetry-collector/main/examples/k8s/otel-collector.yaml
-echo "OpenTelemetry collector deployed."
--- express-t2s-app-v5/sre/reliability/slo-burn-check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Check Datadog burn rate of an SLO
-
-SLO_ID=$1
-
-curl -s \
-"https://api.datadoghq.com/api/v1/slo/$SLO_ID/burn_rate?thresholds=7d,1h" \
--H "DD-API-KEY: $DD_API_KEY" \
--H "DD-APPLICATION-KEY: $DD_APP_KEY"
